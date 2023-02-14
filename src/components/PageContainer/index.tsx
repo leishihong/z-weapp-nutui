14447d9ff758f2570c1df96be38d29ed42ddad99
beichen <leishihong@jd.com>
2023-02-14 14:46:24 +0800

    fix: eslint

--- src/components/PageContainer/index.tsx
+++ src/components/PageContainer/index.tsx
@@ -1,25 +1,17 @@
-import {
-	FC,
-	memo,
-	ReactNode,
-	useState,
-	useMemo,
-	useEffect,
-	useCallback
-} from 'react';
+import { FC, memo, ReactNode, useState, useMemo, useEffect, useCallback } from 'react';
 import { View, Block, Image } from '@tarojs/components';
 import { useSelector, useDispatch } from 'react-redux';
 import Taro, {
-	showModal,
-	showToast,
-	useDidShow,
-	usePageScroll,
-	useTabItemTap,
-	useReady,
-	useShareAppMessage,
-	ShareAppMessageObject,
-	useDidHide,
-	nextTick
+  showModal,
+  showToast,
+  useDidShow,
+  usePageScroll,
+  useTabItemTap,
+  useReady,
+  useShareAppMessage,
+  ShareAppMessageObject,
+  useDidHide,
+  nextTick
 } from '@tarojs/taro';
 import cls from 'classnames';
 
@@ -27,154 +19,141 @@ import { TaroNavigationBar } from 'components/index';
 import { UPopup } from 'taste-ui/index';
 import { NavigationBarProps } from 'components/TaroNavigationBar/type';
 import { TXReverseGeocoder } from 'utils/TX-map';
+import { storageCache } from 'utils/storageCache';
 
 import cx from './index.module.scss';
 
 interface IProps extends NavigationBarProps {
-	title: string;
-	children: ReactNode;
-	isCustomNavBar: boolean;
-	showNavBar: boolean;
-	selectedIndex: number;
+  title: string;
+  children: ReactNode;
+  isCustomNavBar: boolean;
+  showNavBar: boolean;
+  selectedIndex: number;
 }
 
 const defaultProps = {
-	isCustomNavBar: false,
-	showNavBar: false,
-	selectedIndex: 0
+  isCustomNavBar: false,
+  showNavBar: false,
+  selectedIndex: 0
 } as IProps;
 
 const PageContainer: FC<Partial<IProps>> = (props) => {
-	const {
-		children,
-		title,
-		isImmersive,
-		isCustomNavBar,
-		showNavBar,
-		selectedIndex,
-		...resetProps
-	} = props;
+  const { children, title, isImmersive, isCustomNavBar, showNavBar, selectedIndex, ...resetProps } = props;
 
-	const dispatch = useDispatch();
-	const tabBarState = useSelector(({ tabBarState }) => tabBarState);
-	const { system } = useSelector(({ globalsState }) => globalsState);
-	const { navBarHeight, navBarExtendHeight } = useMemo(() => system, []);
+  const dispatch = useDispatch();
+  const tabBarState = useSelector(({ tabBarState }) => tabBarState);
+  const { system } = useSelector(({ globalsState }) => globalsState);
+  const { navBarHeight, navBarExtendHeight } = useMemo(() => system, []);
 
-	const [navBarInfo, setNavBarInfo] = useState<any>({
-		isImmersive,
-		background: `linear-gradient(180deg, rgba(255, 255, 255, 0.6) 0%, rgba(255, 255, 255, 0) 100%)`,
-		title
-	});
-	useEffect(() => {
-		dispatch({ type: 'tabBarState/setState', payload: { selectedIndex } });
-		getFuzzyLocation();
-	}, []);
+  const [isAuthLocation, setIsAuthLocation] = useState<any>(false);
 
-	const getFuzzyLocation = async () => {
-		try {
-			const locationRes: Taro.getFuzzyLocation.SuccessCallbackResult =
-				await Taro.getFuzzyLocation({ type: 'gcj02' });
-			if (locationRes?.errMsg.indexOf('ok') !== -1) {
-				const { status, result }: any = await TXReverseGeocoder(locationRes);
-				if (status === 0) {
-					console.log(result, 'result');
-				}
-			}
-		} catch (error: any) {
-			if (error?.errMsg.indexOf('deny') !== -1) {
-				console.log('用户拒绝授权定位');
-			}
-			console.log(error, '---');
-		}
-	};
-	useDidHide(() => {
-		handleClosePopup();
-	});
-	usePageScroll((payload) => {
-		const { scrollTop } = payload;
-		let navOp = scrollTop / (navBarHeight + navBarExtendHeight);
-		if (isCustomNavBar && showNavBar) {
-			setNavBarInfo((preState) =>
-				Object.assign({}, preState, {
-					background:
-						scrollTop > 10
-							? `rgba(255, 255, 255, ${navOp > 1 ? 1 : navOp})`
-							: 'linear-gradient(180deg, rgba(255, 255, 255, 0.6) 0%, rgba(255, 255, 255, 0) 100%)',
-					title: scrollTop > 10 ? title : ''
-				})
-			);
-		}
-	});
+  console.log(isAuthLocation, 'isAuthLocation');
 
-	const handleClosePopup = useCallback(() => {
-		dispatch({
-			type: 'tabBarState/setState',
-			payload: { popupVisible: false, showTabBar: false }
-		});
-	}, []);
+  const [navBarInfo, setNavBarInfo] = useState<any>({
+    isImmersive,
+    background: `linear-gradient(180deg, rgba(255, 255, 255, 0.6) 0%, rgba(255, 255, 255, 0) 100%)`,
+    title
+  });
+  useEffect(() => {
+    dispatch({ type: 'tabBarState/setState', payload: { selectedIndex } });
+    getFuzzyLocation();
+    getUserAuthLocation();
+  }, []);
+  const getUserAuthLocation = async () => {
+    const res = await storageCache('userAuthLocation');
+    console.log(res, 'userAuthLocation');
+    setIsAuthLocation(['reject', 'fail'].includes(res) && res);
+  };
 
-	return (
-		<Block>
-			<TaroNavigationBar
-				{...resetProps}
-				{...navBarInfo}
-				r-if={isCustomNavBar && showNavBar}
-			/>
-			{children}
-			<UPopup
-				visible={tabBarState.popupVisible}
-				placement="bottom"
-				rounded
-				maskClosable
-				showToolbar={false}
-				footer={null}
-				onCancel={handleClosePopup}
-			>
-				<View className={cx['publish']}>
-					<View className={cx['publish-body']}>
-						<View className={cx['publish-grid']}>
-							<View className={cx['publish-grid-avatar']}>
-								<Image
-									src="https://s1.ax1x.com/2023/02/13/pSo8yOs.png"
-									lazyLoad
-									mode="aspectFill"
-								/>
-							</View>
-							<View className={cx['publish-grid-desc']}>
-								<View className={cx['publish-grid--desc-title']}>创建社团</View>
-								<View className={cx['publish-grid--desc-subtitle']}>
-									仅限个人参加
-								</View>
-							</View>
-						</View>
-						<View className={cx['publish-grid']}>
-							<View className={cx['publish-grid-avatar']}>
-								<Image
-									src="https://s1.ax1x.com/2023/02/13/pSo8syj.png"
-									lazyLoad
-									mode="aspectFill"
-								/>
-							</View>
-							<View className={cx['publish-grid-desc']}>
-								<View className={cx['publish-grid--desc-title']}>发布活动</View>
-								<View className={cx['publish-grid--desc-subtitle']}>
-									分享我的趣味生活
-								</View>
-							</View>
-						</View>
-					</View>
-					<View className={cx['publish-close']} onClick={handleClosePopup}>
-						<Image
-							src="https://s1.ax1x.com/2023/02/13/pSo8cmn.png"
-							lazyLoad
-							mode="aspectFill"
-						/>
-					</View>
-				</View>
-			</UPopup>
-		</Block>
-	);
+  const getFuzzyLocation = async () => {
+    try {
+      const locationRes: Taro.getFuzzyLocation.SuccessCallbackResult = await Taro.getFuzzyLocation({ type: 'gcj02' });
+      if (locationRes?.errMsg.indexOf('ok') !== -1) {
+        const { status, result }: any = await TXReverseGeocoder(locationRes);
+        if (status === 0) {
+          console.log(result, 'result');
+          await storageCache('userAuthLocation', { value: result, cacheTime: 36000 * 24 * 7 });
+        }
+      }
+    } catch (error: any) {
+      if (error?.errMsg.indexOf('deny') !== -1) {
+        console.log('用户拒绝授权定位');
+        await storageCache('userAuthLocation', { value: 'reject' });
+      } else {
+        await storageCache('userAuthLocation', { value: 'fail' });
+      }
+      console.log(error, '---');
+    }
+  };
+  useDidHide(() => {
+    handleClosePopup();
+  });
+  usePageScroll((payload) => {
+    const { scrollTop } = payload;
+    const navOp = scrollTop / (navBarHeight + navBarExtendHeight);
+    if (isCustomNavBar && showNavBar) {
+      setNavBarInfo((preState) =>
+        Object.assign({}, preState, {
+          background:
+            scrollTop > 10
+              ? `rgba(255, 255, 255, ${navOp > 1 ? 1 : navOp})`
+              : 'linear-gradient(180deg, rgba(255, 255, 255, 0.6) 0%, rgba(255, 255, 255, 0) 100%)',
+          title: scrollTop > 10 ? title : ''
+        })
+      );
+    }
+  });
+
+  const handleClosePopup = useCallback(() => {
+    dispatch({
+      type: 'tabBarState/setState',
+      payload: { popupVisible: false, showTabBar: false }
+    });
+  }, []);
+
+  return (
+    <Block>
+      <TaroNavigationBar {...resetProps} {...navBarInfo} r-if={isCustomNavBar && showNavBar} />
+      {children}
+      <UPopup
+        visible={tabBarState.popupVisible}
+        placement='bottom'
+        rounded
+        maskClosable
+        showToolbar={false}
+        footer={null}
+        onCancel={handleClosePopup}
+      >
+        <View className={cx['publish']}>
+          <View className={cx['publish-body']}>
+            <View className={cx['publish-grid']}>
+              <View className={cx['publish-grid-avatar']}>
+                <Image src='https://s1.ax1x.com/2023/02/13/pSo8yOs.png' lazyLoad mode='aspectFill' />
+              </View>
+              <View className={cx['publish-grid-desc']}>
+                <View className={cx['publish-grid--desc-title']}>创建社团</View>
+                <View className={cx['publish-grid--desc-subtitle']}>仅限个人参加</View>
+              </View>
+            </View>
+            <View className={cx['publish-grid']}>
+              <View className={cx['publish-grid-avatar']}>
+                <Image src='https://s1.ax1x.com/2023/02/13/pSo8syj.png' lazyLoad mode='aspectFill' />
+              </View>
+              <View className={cx['publish-grid-desc']}>
+                <View className={cx['publish-grid--desc-title']}>发布活动</View>
+                <View className={cx['publish-grid--desc-subtitle']}>分享我的趣味生活</View>
+              </View>
+            </View>
+          </View>
+          <View className={cx['publish-close']} onClick={handleClosePopup}>
+            <Image src='https://s1.ax1x.com/2023/02/13/pSo8cmn.png' lazyLoad mode='aspectFill' />
+          </View>
+        </View>
+      </UPopup>
+    </Block>
+  );
 };
+
 PageContainer.defaultProps = defaultProps;
 
 export default memo(PageContainer);
--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -1,16 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { View, Image } from '@tarojs/components';
 import { useSelector, useDispatch } from 'react-redux';
 import Taro from '@tarojs/taro';
 import cls from 'classnames';
 
-import {
-	PageContainer,
-	PKVoting,
-	AdvertiseArea,
-	ActivityCard,
-	CommunityCard
-} from 'components/index';
+import { PageContainer, PKVoting, AdvertiseArea, ActivityCard, CommunityCard } from 'components/index';
 import { UTabs, UCard, Avatar } from 'taste-ui/index';
 
 import { storageCache } from 'utils/storageCache';
@@ -22,84 +16,79 @@ const bme = UWithNaming({ n: 'home-' });
 const b = bme('page');
 
 const HomePage = () => {
-	const [tabActive, setTabActive] = useState<string | number>('');
-	const handleTabClick = (tabItem) => {
-		console.log(tabItem, 'tabItem');
-		setTabActive(tabItem.id);
-	};
+  const [tabActive, setTabActive] = useState<string | number>('');
+  const handleTabClick = (tabItem) => {
+    console.log(tabItem, 'tabItem');
+    setTabActive(tabItem.id);
+  };
 
-	return (
-		<PageContainer
-			title=""
-			isImmersive
-			isCustomNavBar
-			showNavBar
-			selectedIndex={0}
-		>
-			<View className={cx[b('')]}>
-				<View className={cx[b('top')]}></View>
-				<View className={cx[b('body')]}>
-					<ActivityCard
-						title="热门推荐"
-						dataSource={[{ title: '活动名称占位最多显示两行字符活动名称...' }]}
-					/>
-					<AdvertiseArea capsuleType="1" />
-					<PKVoting type="PK" />
-					<PKVoting type="VOTE" style={{ height: '436rpx' }} />
-					<UCard className={cx[b('knowledge')]} showCardHeader={false}>
-						<Avatar
-							src="https://img.mtaste.cn/test/img/system/knowledge/e0bd4436fa464088979194ef848ac4ff.jpg"
-							size={[686, 400]}
-							shape="rounded"
-						/>
-						<View className={cls(cx[b('knowledge-title')], 'ellipsis')}>
-							【ozzon上新】花鸟龙月生成色怎么搭配
-						</View>
-					</UCard>
-					<CommunityCard
-						title="社团"
-						dataSource={[
-							{
-								title:
-									'活动名称占位最多显示两行字符活动名称活动名称占位最多显示两行字符活动名称活动名称占位最多显示两行字符活动名称活动名称占位最多显示两行字符活动名称活动名称占位最多显示两行字符活动名称...'
-							},
-							{ title: '活动名称占位最多显示两行字符活动名称...' }
-						]}
-						showMoreIcon
-					/>
-					<ActivityCard
-						title="找活动"
-						dataSource={[
-							{
-								title:
-									'活动名称占位最多显示两行字符活动名称活动名称占位最多显示两行字符活动名称活动名称占位最多显示两行字符活动名称活动名称占位最多显示两行字符活动名称活动名称占位最多显示两行字符活动名称...'
-							},
-							{ title: '活动名称占位最多显示两行字符活动名称...' },
-							{ title: '活动名称占位最多显示两行字符活动名称...' },
-							{ title: '活动名称占位最多显示两行字符活动名称...' },
-							{ title: '活动名称占位最多显示两行字符活动名称...' },
-							{ title: '活动名称占位最多显示两行字符活动名称...' },
-							{ title: '活动名称占位最多显示两行字符活动名称...' }
-						]}
-						showMoreIcon
-					>
-						<UTabs
-							dataSource={[
-								{ id: '', title: '全部' },
-								{ id: '1', title: '漫展' },
-								{ id: '2', title: '国风' },
-								{ id: '3', title: '新鲜' }
-							]}
-							value={tabActive}
-							valueKey="id"
-							valueLabel="title"
-							onClick={handleTabClick}
-						/>
-					</ActivityCard>
-				</View>
-			</View>
-		</PageContainer>
-	);
+  return (
+    <PageContainer title='' isImmersive isCustomNavBar showNavBar selectedIndex={0}>
+      <View className={cx[b('')]}>
+        <View className={cx[b('top')]}></View>
+        <View className={cx[b('body')]}>
+          <ActivityCard title='热门推荐' dataSource={[{ title: '活动名称占位最多显示两行字符活动名称...' }]} />
+          <AdvertiseArea capsuleType='1' />
+          <PKVoting type='PK' />
+          <PKVoting type='VOTE' style={{ height: '436rpx' }} />
+          <UCard className={cx[b('knowledge')]} showCardHeader={false}>
+            <Avatar
+              src='https://img.mtaste.cn/test/img/system/knowledge/e0bd4436fa464088979194ef848ac4ff.jpg'
+              size={[686, 400]}
+              shape='rounded'
+            />
+            <View className={cls(cx[b('knowledge-title')], 'ellipsis')}>【ozzon上新】花鸟龙月生成色怎么搭配</View>
+          </UCard>
+          <CommunityCard
+            title='社团'
+            dataSource={[
+              {
+                title:
+                  '活动名称占位最多显示两行字符活动名称活动名称占位最多显示两行字符活动名称活动名称占位最多显示两行字符活动名称活动名称占位最多显示两行字符活动名称活动名称占位最多显示两行字符活动名称...'
+              },
+              { title: '活动名称占位最多显示两行字符活动名称...' }
+            ]}
+            showMoreIcon
+          />
+          <ActivityCard
+            title='找活动'
+            dataSource={[
+              {
+                title:
+                  '活动名称占位最多显示两行字符活动名称活动名称占位最多显示两行字符活动名称活动名称占位最多显示两行字符活动名称活动名称占位最多显示两行字符活动名称活动名称占位最多显示两行字符活动名称...'
+              },
+              { title: '活动名称占位最多显示两行字符活动名称...' },
+              { title: '活动名称占位最多显示两行字符活动名称...' },
+              { title: '活动名称占位最多显示两行字符活动名称...' },
+              { title: '活动名称占位最多显示两行字符活动名称...' },
+              { title: '活动名称占位最多显示两行字符活动名称...' },
+              { title: '活动名称占位最多显示两行字符活动名称...' }
+            ]}
+            showMoreIcon
+          >
+            <UTabs
+              dataSource={[
+                { id: '', title: '全部' },
+                { id: '1', title: '漫展' },
+                { id: '2', title: '国风' },
+                { id: '3', title: '新1鲜' },
+                { id: '11', title: '漫2展' },
+                { id: '22', title: '国3风' },
+                { id: '33', title: '新412212鲜' },
+                { id: '131', title: '漫1sdsdsd2展' },
+                { id: '242', title: 'sdsd' },
+                { id: '353', title: '新sdsd4鲜' }
+              ]}
+              value={tabActive}
+              valueKey='id'
+              valueLabel='title'
+              onClick={handleTabClick}
+            />
+          </ActivityCard>
+        </View>
+      </View>
+    </PageContainer>
+  );
 };
 
 export default HomePage;
